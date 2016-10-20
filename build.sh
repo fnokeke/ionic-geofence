c6a9a27b4ce37334bdb27f21a7796e696005ec5e
Fabian Okeke <fnokeke@gmail.com>
2016-10-20 15:16:50 -0400

    updated build script

--- build.sh
+++ build.sh
@@ -15,12 +15,12 @@ echo "Now copying android to remote server..."
 echo "*************************"
 # scp /Users/fnokeke/dev/ionic-apps/ionic-geofence/platforms/android/build/outputs/apk/android-debug.apk fabian@slm.smalldata.io:~/mobile_apps
 
-
 echo "*************************"
 echo "Now launching xcode project and itunes connect website"
 echo "*************************"
 open "/Users/fnokeke/dev/ionic-apps/ionic-geofence/platforms/ios/Geo Places.xcodeproj"
 open "https://itunesconnect.apple.com"
+open "https://drive.google.com/drive/u/1/folders/0Bw3oSo0M3Ec9Rlc3SmRlY2hCRUU"
 
 
 echo "*************************"
